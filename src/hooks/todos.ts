c0213590b61477fbfb84d0a5ed12e5e56683ed2a
jonreading81 <jon.reading.email@gmail.com>
2023-01-12 12:05:45 +0000

    chore: remove unneeded try catch blocks

--- src/hooks/todos.ts
+++ src/hooks/todos.ts
@@ -5,50 +5,38 @@ export const useTodos = (initalValue: todosService.Todo[]) => {
   const [todos, setTodos] = useState<todosService.Todo[]>(initalValue);
 
   const addTodo = async (name: todosService.Todo["name"]) => {
-    try {
-      const { data: createdTodo, errors: createdTodoErrors } =
-        await todosService.createTodo(name);
+    const { data: createdTodo, errors: createdTodoErrors } =
+      await todosService.createTodo(name);
 
-      if (createdTodo) {
-        setTodos([createdTodo, ...todos]);
-      } else {
-        console.log({ createdTodoErrors });
-      }
-    } catch (error) {
-      console.log(error);
+    if (createdTodo) {
+      setTodos([createdTodo, ...todos]);
+    } else {
+      console.log({ createdTodoErrors });
     }
   };
 
   const deleteTodo = async (id: string) => {
-    try {
-      const { data, errors } = await todosService.deleteTodo(id);
-      if (errors) {
-        console.log(errors);
-      } else {
-        setTodos(todos.filter((element) => element._id !== id));
-      }
-    } catch (error) {
-      console.log(error);
+    const { data, errors } = await todosService.deleteTodo(id);
+    if (errors) {
+      console.log(errors);
+    } else {
+      setTodos(todos.filter((element) => element._id !== id));
     }
   };
 
   const toggleStatusTodo = async (todo: todosService.Todo) => {
-    try {
-      const { data, errors } = await todosService.toggleStatusTodo(todo);
-      if (data) {
-        setTodos(
-          todos.map((element) => {
-            if (element._id === todo._id) {
-              element = data;
-            }
-            return element;
-          })
-        );
-      } else {
-        console.log(errors);
-      }
-    } catch (error) {
-      console.log(error);
+    const { data, errors } = await todosService.toggleStatusTodo(todo);
+    if (data) {
+      setTodos(
+        todos.map((element) => {
+          if (element._id === todo._id) {
+            element = data;
+          }
+          return element;
+        })
+      );
+    } else {
+      console.log(errors);
     }
   };
 
